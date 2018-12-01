fb3b1c3c2b32d7f14e64b76f043350f455989fbe
sarvani.upadrasta <sarvani.upadrasta@servicenow.com>
2018-12-01 22:29:54 +0530

    Usage of props

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            header: "Header from props...",
+            content: "Content from props..."
+        }
+    }
    render(){
       return(
          <div>
             <h1>Hello React</h1>
+            <h2>{this.props.renderArg1}</h2>
+            <h2>{this.props.renderArg2}</h2>
+            <h2>{this.props.defaultArg1}</h2>
+            <h2>{this.props.defaultArg2}</h2>
+            <Component1 headerProp = {this.state.header}/>
          </div>
       );
    }
+}
+class Component1 extends Component{
+    render(){
+        return(
+            <div>
+                <h1>{this.props.headerProp}</h1>
+            </div>
+        );
+    }
+}
+
+App.defaultProps = {
+    defaultArg1:"App default argument1",
+    defaultArg2:"App default argument2"
 }
 export default App;
--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App renderArg1="Rendering element1 using prop" renderArg2="Rendering element2 using prop"/>, document.getElementById('app'));
