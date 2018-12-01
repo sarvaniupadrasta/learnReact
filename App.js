4c6323dcb9382f8618e6dee9349fdbc7e02d5f0a
sarvani.upadrasta <sarvani.upadrasta@servicenow.com>
2018-12-01 22:06:32 +0530

    state gets the data

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 class App extends Component{
+    constructor(){
+        super();
+        this.header = "Header from state";
+        this.content = "Content from state";
+    }
    render(){
       return(
          <div>
-            <h1>Hello React</h1>
+            <h2>{this.header}</h2>
+            <h2>{this.content}</h2>
          </div>
       );
    }
