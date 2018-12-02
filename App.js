eb7f5876c66d46277f296059668e30e0ff2f3bb4
sarvani.upadrasta <sarvani.upadrasta@servicenow.com>
2018-12-02 10:40:22 +0530

    stateChange and DOM ChangeHandlers

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends Component{
+    constructor() {
+        super();
+        this.state = {
+            data:["state1"]
+        };
+        this.setStateHandler = this.setStateHandler.bind(this);
+    };
+    setStateHandler() {
+        var item = "setState";
+        var myArray = [];
+        myArray = this.state.data.slice();
+        myArray.push(item);
+        console.log(myArray);
+        this.setState({data:myArray});
+    };
+    ChangeDomHandler() {
+        var myDiv = document.getElementById('myDiv');
+        console.log(myDiv);
+        console.log(ReactDOM.findDOMNode(myDiv));
+        //myDiv.style.color = "red";
+        ReactDOM.findDOMNode(myDiv).style.color = "red";
+    }
    render(){
       return(
          <div>
-            <h1>Hello React</h1>
+             <button onClick={this.setStateHandler}>setState</button>
+             <h2>{this.state.data}</h2>
+             <h3>{Math.random()}</h3>
+             <button onClick={this.ChangeDomHandler}>Change Color</button>
+             <div id="myDiv">
+              Color Changed
+             </div>
          </div>
       );
    }
